ef0e9a7d43e3ac9692c3fda27fcf45e02cd7d851
Suriyaa Sundararuban <github@suriyaa.tk>
2024-06-24 16:38:28 +0200

    Cleanup test_jekyll_build.rb

--- tests/test_jekyll_build.rb
+++ tests/test_jekyll_build.rb
@@ -1,22 +1,9 @@
 # Build Test for Rakefile written in Ruby
 require 'minitest/autorun'
-require 'fileutils'
 
 class TestJekyllBuild < Minitest::Test
-  def setup
-    @site_dir = "_site"
-    @index_file = File.join(@site_dir, "index.html")
-  end
-
-  def test_site_directory_exists
-    assert Dir.exist?(@site_dir), "The _site directory should exist after Jekyll build."
-  end
-
-  def test_index_file_exists
-    assert File.exist?(@index_file), "The index.html file should exist in the _site directory."
-  end
-
-  def teardown
-    FileUtils.rm_rf(@site_dir) if Dir.exist?(@site_dir)
+  def test_jekyll_build_exits_successfully
+    result = `jekyll build`
+    assert $?.success?, "Jekyll build command did not exit successfully.\nOutput:\n#{result}"
   end
 end
